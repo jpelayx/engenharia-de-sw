cd3700c709953f4032daf5419ae555d6aab92243
TheCapraSlayer <72527278+TheCapraSlayer@users.noreply.github.com>
2022-08-12 09:58:33 -0300

    Alguns htmls implementados, chegando mais próximo da concretização de um caso de uso

--- Mobi Campus/MobiCampus/MobiCampus/models.py
+++ Mobi Campus/MobiCampus/MobiCampus/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 max_passageiros=4
 max_tam_string=50
+max_tam_senha=20
 tam_cnh=11
 max_rota=max_tam_string*3
 
@@ -11,9 +12,10 @@ class Usuario(models.Model):
     nome=models.CharField(max_length=max_tam_string)
     validado=models.BooleanField(default=False)
     aval=models.IntegerField(default=5)
+    senha=models.CharField(max_length=max_tam_senha)
     
     def __str__(self):
-        return self.login
+        return self.nome
 
 class Administrador(Usuario):
     permissao=models.BooleanField()
--- Mobi Campus/MobiCampus/MobiCampus/views.py
+++ Mobi Campus/MobiCampus/MobiCampus/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Usuario, Administrador, Motorista
 from django.template import loader
+from django.http import Http404
 
 def index(request):
     user_list = Usuario.objects.order_by('-nome')
@@ -12,8 +13,14 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def detail(request, usuario_login):
-    user= Usuario.objects.get(pk=usuario_login)
-    return HttpResponse("Voce esta olhando para o user %s" % usuario_login)
+    user=get_object_or_404(Usuario, pk=usuario_login)
+    
+    template = loader.get_template('MobiCampus/detail.html')
+    contexto={
+        'user':user,
+    }
+
+    return HttpResponse(template.render(contexto, request))
 
 def results(request, usuario_login):
     resposta= "Voce esta olhando para o nome do usuario %s"
